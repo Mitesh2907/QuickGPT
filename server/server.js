3b381bc4c63e5240fce9ef3b63876124c7d64e36
Mitesh2907 <miteshnayi081@gmail.com>
2025-12-08 23:05:20 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,43 +1,40 @@
 import express from 'express'
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './configs/db.js'
+
 import userRouter from './routes/userRoutes.js'
 import chatRouter from './routes/chatRoutes.js'
 import messageRouter from './routes/messageRoutes.js'
 import paymentRoutes from "./routes/paymentRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
 
-
-
-const app = express()
-
-await connectDB()
-
-//Middleware
-app.use(cors())
-app.use(express.json())
-
-//Routes
-app.get('/', (req,res)=>res.send('Server is Live!'))
-app.use('/api/user',userRouter)
-app.use('/api/chat', chatRouter)
-app.use('/api/message', messageRouter )
-app.use("/api/payment", paymentRoutes);
-app.use("/api/admin", adminRoutes);
-
-// server.js (top)
-import dotenv from "dotenv";
 dotenv.config();
 
+const app = express();
+
+// Database
+await connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Debugging
 console.log("cwd:", process.cwd());
 console.log("ENV ADMIN_USERNAME:", process.env.ADMIN_USERNAME);
 console.log("ENV ADMIN_PASSWORD:", process.env.ADMIN_PASSWORD);
 
+// Routes
+app.get('/', (req, res) => res.send('Server is Live!'));
+app.use('/api/user', userRouter);
+app.use('/api/chat', chatRouter);
+app.use('/api/message', messageRouter);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/admin", adminRoutes);
 
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, ()=>{ 
-    console.log(`Server is running on port ${PORT}`)
-})
+// Server Listen
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
